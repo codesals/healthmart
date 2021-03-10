635298c56de4a876d830c0888987cf4294a2bdcc
Sally <78157471+codesals@users.noreply.github.com>
2021-03-10 20:16:30 +0300

    Use axios to fetch products from backend Task 14 (React) Completed

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -1,8 +1,9 @@
-import products from "../products";
 // import { action, makeObservable, observable } from "mobx";
 import { makeAutoObservable } from "mobx";
+import axios from "axios";
+
 class ProductStore {
-  products = products;
+  products = [];
 
   constructor() {
     // makeObservable(this, {
@@ -16,8 +17,18 @@ class ProductStore {
   deleteProduct = (productId) => {
     this.products = this.products.filter((product) => product.id !== productId);
   };
+
+  fetchProducts = async () => {
+    try {
+      const res = await axios.get("http://localhost:8000/products");
+      this.products = res.data;
+    } catch (error) {
+      console.error("ProductStore -> fetchProducts -> error", error);
+    }
+  };
 }
 
 const productStore = new ProductStore();
+productStore.fetchProducts();
 
 export default productStore;
